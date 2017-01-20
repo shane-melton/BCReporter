9e2ec187a4f353e367f94dc7bbdc1074e9ba44d7
David Vaughan <DavidRayVaughan00@gmail.com>
2017-01-19 22:46:13 -0800

    Add cross-application rule capabilities

--- rule_based_fraud_detector.py
+++ rule_based_fraud_detector.py
@@ -1,7 +1,8 @@
 import operator as op
 
+
 class RuleBasedFraudDetector:
-    operators = {
+    _operators = {
         '<': op.lt,
         '<=': op.le,
         '==': op.eq,
@@ -10,20 +11,42 @@ class RuleBasedFraudDetector:
         '!=': op.ne
     }
 
-    def __init__(self):
-        pass
+    _aggregations = {
+        'count': lambda column, key, apps: len(
+            (x for x in (app[column] for app in apps) if x == key))
+    }
 
-    def satisfies_condition(self, condition, lhs, rhs):
-        return self.operators[condition](lhs, rhs)
+    def __init__(self, single_app_rules=dict(), cross_app_rules=dict()):
+        self._single_app_rules = single_app_rules
+        self._cross_app_rules = cross_app_rules
 
-    def apply_rule(self, column, condition, value, application):
-        if not self.satisfies_condition(condition, application[column], value):
+    def _satisfies_condition(self, condition, lhs, rhs):
+        return self._operators[condition](lhs, rhs)
+
+    def _apply_single_app_rule(self, column, condition, value, app):
+        if not self._satisfies_condition(condition, app[column], value):
             return (condition, column, value)
         return None
 
-    def apply_rules(self, rules, application):
-        violations = []
-        for rule in rules:
-            result = self.apply_rule(rule['column'], rule['condition'], rule['value'], application)
-            if result: violations.append(result)
-        return violations
+    def _apply_cross_app_rule(self, aggregation, column, condition, value, apps, **kwargs):
+        satisfies = self._satisfies_condition(
+            condition, _aggregations[aggregation](column, kwargs['key'], apps), value):
+        if not satisfies:
+            return (condition, aggregation, kwargs['key'], column, value)
+        return None
+
+    def apply_rules(self, apps):
+        single_app_violations = []
+        cross_app_violations = []
+
+        for rule in self._single_app_rules:
+            for app in enumerate(apps):
+                result = self._apply_single_app_rule(**rule, app=app[1])
+                if result: single_app_violations.append((app[0], **result))
+
+        for rule in self._cross_app_rules:
+            result = self._apply_cross_app_rule(**rule, apps=apps)
+            if result: cross_app_violations.append(result)
+
+        return (single_app_violations, cross_app_violations)
+
