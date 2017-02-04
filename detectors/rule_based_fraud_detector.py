0c4334220cb4fc4a2dfdfe464eb0fb3c6e24bb90
David Vaughan <DavidRayVaughan00@gmail.com>
2017-02-04 13:39:31 -0800

    Add detector unittests and fix bugs

--- detectors/rule_based_fraud_detector.py
+++ detectors/rule_based_fraud_detector.py
@@ -2,38 +2,9 @@ import operator as op
 
 
 class RuleBasedFraudDetector:
-    _operators = {
-        '<': op.lt,
-        '<=': op.le,
-        '==': op.eq,
-        '>=': op.ge,
-        '>': op.gt,
-        '!=': op.ne
-    }
-
-    _aggregations = {
-        'count': lambda column, key, apps: len(
-            (x for x in (app[column] for app in apps) if x == key))
-    }
-
     def __init__(self, single_app_rules=dict(), cross_app_rules=dict()):
         self._single_app_rules = single_app_rules
         self._cross_app_rules = cross_app_rules
-
-    def _satisfies_condition(self, condition, lhs, rhs):
-        return self._operators[condition](lhs, rhs)
-
-    def _apply_single_app_rule(self, column, condition, value, app):
-        if not self._satisfies_condition(condition, app[column], value):
-            return (condition, column, value)
-        return None
-
-    def _apply_cross_app_rule(self, aggregation, column, condition, value, apps, **kwargs):
-        satisfies = self._satisfies_condition(
-            condition, _aggregations[aggregation](column, kwargs['key'], apps), value)
-        if not satisfies:
-            return (condition, aggregation, kwargs['key'], column, value)
-        return None
 
     def apply_rules(self, apps):
         single_app_violations = []
@@ -42,11 +13,43 @@ class RuleBasedFraudDetector:
         for rule in self._single_app_rules:
             for app in enumerate(apps):
                 result = self._apply_single_app_rule(**rule, app=app[1])
-                if result: single_app_violations.append(app[0] + result)
+                if result: single_app_violations.append((app[0],) + result)
 
         for rule in self._cross_app_rules:
             result = self._apply_cross_app_rule(**rule, apps=apps)
             if result: cross_app_violations.append(result)
 
         return (single_app_violations, cross_app_violations)
+
+    @staticmethod
+    def _get_operators():
+        return {
+            '<': op.lt,
+            '<=': op.le,
+            '==': op.eq,
+            '>=': op.ge,
+            '>': op.gt,
+            '!=': op.ne}
+
+    @staticmethod
+    def _get_aggregations():
+        return {
+            'count': lambda column, key, apps: len(
+                [x for x in (app[column] for app in apps) if x == key])}
+
+    @classmethod
+    def _satisfies_condition(cls, condition, lhs, rhs):
+        return cls._get_operators()[condition](lhs, rhs)
+
+    def _apply_single_app_rule(self, column, condition, value, app):
+        if not self._satisfies_condition(condition, app[column], value):
+            return (condition, column, value)
+        return None
+
+    def _apply_cross_app_rule(self, aggregation, column, condition, value, apps, **kwargs):
+        satisfies = self._satisfies_condition(
+            condition, self._get_aggregations()[aggregation](column, kwargs['key'], apps), value)
+        if not satisfies:
+            return (condition, aggregation, kwargs['key'], column, value)
+        return None
 
--- tests/test_rule_based_fraud_detector.py
+++ tests/test_rule_based_fraud_detector.py
@@ -4,24 +4,80 @@ from detectors.rule_based_fraud_detector import RuleBasedFraudDetector
 
 class TestRuleBasedFraudDetector(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        print("init")
+        self.single_app_rules = [
+            {
+                'column': 'income',
+                'condition': '>=',
+                'value': 100}]
+        self.cross_app_rules = [
+            {
+                'aggregation': 'count',
+                'column': 'ssn-hash',
+                'condition': '<=',
+                'value': 3,
+                'key': '123'}]
+
+        self.single_good_app = {'income': 110, 'name': 'Bob', 'ssn-hash': '123'}
+        self.single_bad_app = {'income': 90, 'name': 'Sally', 'ssn-hash': '456'}
+
+        self.cross_good_apps = [
+            self.single_good_app,
+            self.single_good_app,
+            self.single_good_app,
+            self.single_bad_app]
+
+        self.cross_bad_apps = self.cross_good_apps + [self.single_good_app]
+
+        self.det = RuleBasedFraudDetector(self.single_app_rules, self.cross_app_rules)
         super(TestRuleBasedFraudDetector, self).__init__(*args, **kwargs)
 
     def test__satisfies_condition(self):
-        # stub
-        pass
+        sat = RuleBasedFraudDetector._satisfies_condition  # For brevity
+        self.assertTrue(sat('<', 0, 1))
+
+        self.assertTrue(sat('<=', 1, 1))
+        self.assertTrue(sat('<=', 0, 1))
+
+        self.assertTrue(sat('==', 1, 1))
+
+        self.assertTrue(sat('>=', 1, 1))
+        self.assertTrue(sat('>=', 1, 0))
+
+        self.assertTrue(sat('>', 1, 0))
+
+        self.assertTrue(sat('!=', 0, 1))
 
     def test__apply_single_app_rule(self):
-        # stub
-        pass
+        self.assertEqual(
+            self.det._apply_single_app_rule(**(self.single_app_rules[0]), app=self.single_good_app),
+            None)
+
+        self.assertEqual(
+            self.det._apply_single_app_rule(**(self.single_app_rules[0]), app=self.single_bad_app),
+            ('>=', 'income', 100))
 
     def test__apply_cross_app_rule(self):
-        # stub
-        pass
+        self.assertEqual(
+            self.det._apply_cross_app_rule(
+                **(self.cross_app_rules[0]),
+                apps=self.cross_good_apps),
+            None)
+
+        self.assertEqual(
+            self.det._apply_cross_app_rule(
+                **(self.cross_app_rules[0]),
+                apps=self.cross_bad_apps),
+            ('<=', 'count', '123', 'ssn-hash', 3))
 
     def test_apply_rules(self):
-        # stub
-        pass
+        self.assertEqual(
+            self.det.apply_rules(self.cross_good_apps),
+            ([(3, '>=', 'income', 100)], []))
+
+        self.assertEqual(
+            self.det.apply_rules(self.cross_bad_apps),
+            ([(3, '>=', 'income', 100)], [('<=', 'count', '123', 'ssn-hash', 3)]))
+
 
 if __name__ == '__main__':
     unittest.main()
